9bbca2ab744bc114571791a5a19285c85fa7782f
Валерий <valery.shumkov@mail.ru>
2025-04-17 23:03:14 +0300

    upd products

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,30 @@
+import Head from "next/head";
 import WelcomeSection from "@/components/Home/WelcomeSection/WelcomeSection";
 import FeatureSection from "@/components/Home/FeatureSection/FeatureSection";
 import BentoGridSection from "@/components/Home/BentoGridSection/BentoGridSection";
-import Feeds from "@/components/Feeds/Feeds";
-
-export const metadata = {
-  title: "Ковры дорожки ковролин",
-  description: "Бесплатная доставка ковровых изделий по Донецкой области",
-  keywords: ["ковры", "дорожки", "ковролин"],
-  openGraph: {
-    title: "Ковры Дорожки Ковролин",
-    description: "Бесплатная доставка ковровых изделий по Донецкой области",
-    type: "website",
-  },
-};
+import ProductListSrction from "@/components/Home/ProductListSection/ProductListSection";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ковры дорожки ковролин</title>
+        <meta
+          name="description"
+          content="Бесплатная доставка ковровых изделий по Донецкой области"
+        />
+        <meta name="keywords" content="ковры, дорожки, ковролин" />
+        <meta property="og:title" content="Ковры Дорожки Ковролин" />
+        <meta
+          property="og:description"
+          content="Бесплатная доставка ковровых изделий по Донецкой области"
+        />
+        <meta property="og:type" content="website" />
+      </Head>
       <WelcomeSection />
       <FeatureSection />
       <BentoGridSection />
-      <Feeds />
+      <ProductListSrction />
     </>
   );
 }
